5f5b734b80765df00964147c533a12cc84f5b611
doll-in-dollars <a-anara@mail.ru>
2016-11-14 02:36:50 +0500

    тест

---
+++ gfgkhhliuj.php
@@ -0,0 +1,10 @@
+<Script type="text/javascript">
+$(document).ready(function(){                 
+	var clickButtonForm=$(".clickButtonForm"); //Делаем выборку кнопки из DOM дерева и заносим ее в перенменную
+	clickButtomForm.click(function(){          //Составляем функцию, которая срабатывает после события click на нажей кнопке
+	var inputform1value = $("#form1");         //Делаем выборку первого чекбокса и заносим ее в переменную
+	var inputform1value = $("#form2");         //Делаем выборку второго чекбокса и заносим ее в переменную
+
+}
+}
+var
